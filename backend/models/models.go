d473570fcd38750bf8e53762176dc077d62620a9
Alexander Ayzin <ayzinalex@gmail.com>
2025-09-05 23:32:24 +0300

    cleanup

--- backend/models/models.go
+++ backend/models/models.go
@@ -1,28 +1,8 @@
 package models
 
-import "time"
-
-type User struct {
-	ID           int
-	Username     string
-	PasswordHash string
-	IsAdmin      bool
-}
-
 type Settings struct {
 	ID                int
 	AllowedExtensions string
 	BlockedExtensions string
 	MaxUploadSizeMB   int
-}
-
-type UploadLog struct {
-	ID          int
-	UserID      int
-	Filename    string
-	Size        float64
-	ContentType string
-	IPAddress   string
-	Status      string
-	Timestamp   time.Time
 }
